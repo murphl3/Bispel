a18e01ce9ed2d8562f6ca58c3f8138e7437907d5
murphl3 <lmurphy3@luc.edu>
2025-04-27 21:07:26 -0500

    Added Super Calls

--- classes/LinkedController3D/PlayerController3D.gd
+++ classes/LinkedController3D/PlayerController3D.gd
@@ -1,2 +1,11 @@
 extends AgentController3D
 class_name PlayerController3D
+
+func _process(body: LinkedBody3D, delta: float) -> void:
+	super._process(body, delta)
+
+func _physics_process(body: LinkedBody3D, delta: float) -> void:
+	super._physics_process(body, delta)
+
+func _integrate_forces(body: LinkedBody3D, state: PhysicsDirectBodyState3D) -> void:
+	super._integrate_forces(body, state)
